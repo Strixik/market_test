86948645226c6ce81a3df75aea2922a4c283cb5b
strixik <strixik1985@gmail.com>
2020-03-29 01:44:35 +0200

    fix

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -40,24 +40,27 @@ class AuthController extends Controller
             return response()->json(['status' => 'success'], 200)->header('Authorization', $token);
         }
 
-        return response()->json([  'status' => 'error', 'errors' => 'Login or password wrong'], 401);
+        return response()->json(['status' => 'error', 'errors' => 'Login or password wrong'], 401);
     }
     public function logout()
     {
         $this->guard()->logout();
         return response()->json([
             'status' => 'success',
-            'msg' => 'Logged out Successfully.'
+            'msg' => 'Logged out Successfully.',
         ], 200);
     }
+
     public function user(Request $request)
     {
         $user = User::find(Auth::user()->id);
+
         return response()->json([
             'status' => 'success',
-            'data' => $user
+            'data' => $user,
         ]);
     }
+
     public function refresh()
     {
         if ($token = $this->guard()->refresh()) {
@@ -65,8 +68,10 @@ class AuthController extends Controller
                 ->json(['status' => 'successs'], 200)
                 ->header('Authorization', $token);
         }
+
         return response()->json(['error' => 'refresh_token_error'], 401);
     }
+
     private function guard()
     {
         return Auth::guard();
